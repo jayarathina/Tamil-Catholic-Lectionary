372a069659e4561811af913a0589471589ed2cf6
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2016-04-14 16:11:07 +0530

    Database Skeleton :+1:
    Just a rough structure... It need to be corrected, updated and
    redesigned...

---
+++ MySQL/scripture__bible.sql
@@ -0,0 +1,11 @@
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scripture__bible`
+--
+
+CREATE TABLE `scripture__bible` (
+  `dayID` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `Content` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
---
+++ MySQL/scripture__sequence.sql
@@ -0,0 +1,11 @@
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scripture__sequence`
+--
+
+CREATE TABLE `scripture__sequence` (
+  `dayID` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `Content` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;

080c1d4647d4a79350958552b42ac1ee8444cacb
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2017-04-25 21:48:19 +0530

    :sweat_drops:

--- example.php
+++ example.php
@@ -1,24 +1,21 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
+header ( 'Content-Type: text/html; charset=utf-8' );
 
 $IS_ADMIN = ! TRUE;
 
 include_once 'lib/TamilLectionary.php';
 include_once 'lib/TamilLectionaryRenderHTML.php';
-
+// @formatter:off
 ?>
 <html>
-	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" type="text/css" href="styles/style_year.css">
-	</head>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" type="text/css" href="styles/style_year.css">
+</head>
 <body>
 <?php
-
-$tamilLect = new TamilLectionary (null, parse_ini_file ( 'settings.ini' ) );
-
-$RenderHTML = new TamilLectionaryRenderHTML();
-
-echo $RenderHTML->renderHTML($tamilLect->rcy);
-
+// @formatter:on
+$tamilLect = new TamilLectionary ( null, parse_ini_file ( 'settings.ini' ) );
+$RenderHTML = new TamilLectionaryRenderHTML ();
+echo $RenderHTML->renderHTML ( $tamilLect->rcy );
 ?>

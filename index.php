0f43ad2f6023dcc4bb92c9ee6c5e299ad1c290ac
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2019-01-04 22:17:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
 <?php
-header ( 'Content-Type: text/html; charset=utf-8' );
-$IS_ADMIN = TRUE;
+header('Content-Type: text/html; charset=utf-8');
 
-include_once 'lib/TamilLectionary.php';
-include_once 'lib/TamilLectionaryRenderHTML.php';
-// @formatter:off
+include_once ('lib/RomanCalendar/RomanCalendar.php');
+include_once ('lib/RomanCalendar/RomanCalendarRenderHTML_TA.php');
 ?>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="styles/style_year.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<meta charset="UTF-8">
-		<meta name="description" content="Daily Mass Reading in Tamil">
-		<meta name="keywords" content="Tamil Lectionary, Tamil Catholic, Tamil Mass Reading">
-		<meta name="author" content="Jayarathina Madarasan">
-		
 	</head>
 <body>
 
@@ -26,14 +19,19 @@ Please not that this is a work in progress.<br/>
 
 
 <?php
-// @formatter:on
-$time_start = microtime ( true );
+// Test Cases
+// 2019 Immaculate conception on sunday
+// 2018 annunciaion during holy week
+// 1967 st joseph during holy week
+// 2017 St. Joseph during lent sunday
+// 2014 Immaculate Hrt coincided with Saint Irenaeus, 28 June
+// 2015 Immaculate Hrt coincided with Saint Anthony of Padua, 13 June
 
-$tamilLect = new TamilLectionary ();
-$filePath = $tamilLect->computeReadings( null, parse_ini_file ( 'settings.ini' ) );
+$CalcGen = new RomanCalendar (2019, parse_ini_file ( 'lib/RomanCalendar/settings.ini' ) );
 
-$RenderHTML = new TamilLectionaryRenderHTML ();
-echo $RenderHTML->renderHTML ($filePath);
+$rcYr = $CalcGen->rcy;
 
-echo 'Total execution time in seconds: ' . (microtime(true) - $time_start);
+$rHTML = new RomanCalendarRenderHTML_TA ();
+$rHTML->printYearHTML ( $rcYr );
+
 ?>

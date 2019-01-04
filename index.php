65ef5664a7e701350a7ba5e8ecd7661e854b3b31
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2019-01-04 22:41:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,21 +11,7 @@ include_once ('lib/RomanCalendar/RomanCalendarRenderHTML_TA.php');
 	</head>
 <body>
 
-Please not that this is a work in progress.<br/>
- 💚 - Denotes Text has been entered in DB<br/>
- 💗 - Text is yet to be entered.<br/>
- 
- You can help too. Please Contact me if you can spare a couple of minutes. All you need to do is copy and paste. 😉
-
-
 <?php
-// Test Cases
-// 2019 Immaculate conception on sunday
-// 2018 annunciaion during holy week
-// 1967 st joseph during holy week
-// 2017 St. Joseph during lent sunday
-// 2014 Immaculate Hrt coincided with Saint Irenaeus, 28 June
-// 2015 Immaculate Hrt coincided with Saint Anthony of Padua, 13 June
 
 $CalcGen = new RomanCalendar (2019, parse_ini_file ( 'lib/RomanCalendar/settings.ini' ) );
 

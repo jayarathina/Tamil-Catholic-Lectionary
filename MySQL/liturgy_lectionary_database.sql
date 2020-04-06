1caf963da18569ec9fe0d596c886f6a32f51a184
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2020-04-06 18:29:43 +0530

    minor

--- MySQL/liturgy_lectionary_database.sql
+++ MySQL/liturgy_lectionary_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2020 at 08:46 AM
+-- Generation Time: Apr 04, 2020 at 08:47 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 

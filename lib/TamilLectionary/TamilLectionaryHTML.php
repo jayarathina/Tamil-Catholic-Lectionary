734e9e0f4dc01b7beee98976c3da8b4ba99d18c7
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2020-05-10 18:16:09 +0530

    Display Alleluia function

--- lib/TamilLectionary/TamilLectionaryHTML.php
+++ lib/TamilLectionary/TamilLectionaryHTML.php
@@ -6,24 +6,42 @@ require_once 'lib/dbConfig.php';
 include_once 'lib/TamilLectionary/TamilLectionaryUtil.php';
 class TamilLectionaryHTML {
 	public $fullYear;
+	public $currYear;
 	private $readingType = [ 
 			'1' => 'முதல் வாசகம்',
 			'2' => 'பதிலுரைப் பாடல்',
 			'3' => 'இரண்டாம் வாசகம்',
 			'4' => '',
-			'5' => '',
+			'5' => 'நற்செய்திக்கு முன் வாழ்த்தொலி',
+			'51'=> 'நற்செய்திக்கு முன் வசனம்',
 			'6' => 'நற்செய்தி வாசகம்',
 			'7' => '',
 			'8' => '',
 			'9' => '' 
 	];
-	function __construct($year) {
-		$this->fullYear = $year;
+	private $database;
+	function __construct($fullYear, $currYear) {
+		$this->fullYear = $fullYear;
+		$this->currYear = $currYear;
+		$this->database = new Medoo ( [ 
+				'database_type' => 'mysql',
+				'database_name' => DB_NAME,
+				'server' => 'localhost',
+				'username' => DB_USER,
+				'password' => DB_PASSWORD,
+				'charset' => 'utf8' 
+		] );
 	}
 	function getDay($date, $month, $evtID = 0) {
-		return $this->getSingleEvent ( $this->fullYear [$month] [$date] [$evtID] );
+		return $this->getSingleEvent ( $this->fullYear [$month] [$date] [$evtID], TamilLectionaryUtil::isItInLent ( $this->currYear, $month, $date ) );
 	}
-	function getSingleEvent($currtDay) {
+	function getSingleEvent($currtDay, $isLent) {
+		
+		// There is a doubt whether Alleluia is sung during lent on Solemnities
+		// Currently we follow Tamil Lectionary, so we sing it on solemnities.
+		if (isset ( $currtDay ['type'] ) && $currtDay ['type'] == 'Solemnity')
+			$isLent = false;
+		
 		$ret = '';
 		
 		for($i = 1; $i < 10; $i ++) {
@@ -43,6 +61,9 @@ class TamilLectionaryHTML {
 				case 2 : // Responsorial
 					$ret .= $this->getResponsorialTxt ( $readings, $currtDay ['code'] );
 					break;
+				case 5 : // Alleluia
+					$ret .= $this->getAlleluiaTxt ( $readings, $currtDay ['code'], $isLent );
+					break;
 				default :
 					print_r ( $readings );
 					break;
@@ -58,20 +79,12 @@ class TamilLectionaryHTML {
 	}
 	
 	function getReadingsTxt($readings, $usedBy = null) {
-		$database = new Medoo ( [ 
-				'database_type' => 'mysql',
-				'database_name' => DB_NAME,
-				'server' => 'localhost',
-				'username' => DB_USER,
-				'password' => DB_PASSWORD,
-				'charset' => 'utf8' 
-		] );
-		
 		$rt = '';
+		// Set Heading
 		$rt .= "<h4 class='clrDay'>{$this->readingType[ intval( array_key_first ( $readings ) ) ]}</h4>";
 		
 		foreach ( $readings as $readngType => $value ) {
-			$readingTxt = $database->get ( 'readings__text', '*', [ 
+			$readingTxt = $this->database->get ( 'readings__text', '*', [ 
 					'refKey' => $value,
 					'usedBy' => $usedBy 
 			] );
@@ -85,72 +98,58 @@ class TamilLectionaryHTML {
 			$rdCnt .= "<p class='readingTxt'>" . str_replace ( '§', '</p><p class="readingTxt">', $readingTxt ['Content'] ) . "</p>";
 			$rdCnt .= "<p class='readingTxt'>ஆண்டவரின் அருள்வாக்கு.</p>";
 			
-			//find out if alternative readings are available, and make them so 
-			preg_match ( '/^\d\.(\d)$/', $readngType, $matches, PREG_OFFSET_CAPTURE );
-			if (! empty ( $matches )) {
-				if ($matches [1] [0] !== '1') {
-					$rdCnt = "<hr class='clrDay'/><h4 class='clrDay italics'>அல்லது</h4>" . $rdCnt;
-				}
+			// find out if alternative readings are available, and make them so
+			if(isset($readngType[3]) && intval($readngType[3]) == 1){
+				$rdCnt = "<hr class='clrDay'/><h4 class='clrDay italics'>அல்லது குறுகிய வாசகம்</h4>" . $rdCnt;
+			}elseif(isset($readngType[2]) && intval($readngType[2]) > 1){
+				$rdCnt = "<hr class='clrDay'/><h4 class='clrDay italics'>அல்லது</h4>" . $rdCnt;
 			}
-			
-			//find out if short readings are available, and make them so 
-			preg_match ( '/^\d\.\d(\d)$/', $readngType, $matches, PREG_OFFSET_CAPTURE );
-			if (! empty ( $matches )) {
-				if ($matches [1] [0] !== '1')
-					$rdCnt = "<hr class='clrDay'/><h4 class='clrDay italics'>அல்லது குறுகிய வாசகம்</h4>" . $rdCnt;
-			}
+
 			$rt .= $rdCnt;
 		}
 		return $rt;
 	}
 	
 	function getResponsorialTxt($readings, $usedBy = null) {
-		$database = new Medoo ( [
-				'database_type' => 'mysql',
-				'database_name' => DB_NAME,
-				'server' => 'localhost',
-				'username' => DB_USER,
-				'password' => DB_PASSWORD,
-				'charset' => 'utf8'
-		] );
-		
 		$rt = '';
+		
+		// Set Heading
 		$rt .= "<h4 class='clrDay'>{$this->readingType[ intval( array_key_first ( $readings ) ) ]}</h4>";
 		
 		foreach ( $readings as $readngType => $value ) {
-			$readingTxt = $database->get ( 'readings__text', '*', [
+			$readingTxt = $this->database->get ( 'readings__text', '*', [ 
 					'refKey' => $value,
-					'usedBy' => $usedBy
+					'usedBy' => $usedBy 
 			] );
 			
-			//Give spaing to verses
+			// Give spaing to verses
 			$refTxt = TamilLectionaryUtil::formatVerseToPrint ( $value );
 			
-			//If alternative response is set (அல்லது:...), then extract it.
+			// If alternative response is set (அல்லது:...), then extract it.
 			$altResponse = '';
-			preg_match('/(.*)\( *அல்லது *:?(.*?)\)/', $readingTxt['introRes'], $matches);
-			if(!empty($matches)){
-				$readingTxt['introRes'] = $matches[1];
-				$altResponse =  "<br/><span class='clrDay'>அல்லது :</span>" . $matches[2];
+			preg_match ( '/(.*)\( *அல்லது *:?(.*?)\)/', $readingTxt ['introRes'], $matches );
+			if (! empty ( $matches )) {
+				$readingTxt ['introRes'] = $matches [1];
+				$altResponse = "<br/><span class='clrDay'>அல்லது :</span>" . $matches [2];
 			}
-
-			//Seperate the response verse ref from responce text.
-			$responseTxt = $readingTxt['introRes'];
+			
+			// Seperate the response verse ref from responce text.
+			$responseTxt = $readingTxt ['introRes'];
 			$responseRef = '';
-			preg_match('/(.*)§(.*)/', $readingTxt['introRes'], $matches);
-			if(!empty($matches)){
-				$responseTxt = $matches[1];
-				$responseRef = ' (பல்லவி: ' . $matches[2] .')';
+			preg_match ( '/(.*)§(.*)/', $readingTxt ['introRes'], $matches );
+			if (! empty ( $matches )) {
+				$responseTxt = $matches [1];
+				$responseRef = " (பல்லவி: $matches[2])";
 			}
 			
 			$rdCnt = '';
-			$rdCnt .= '<span class="clrDay italics">' . $refTxt . $responseRef .'</span>';
+			$rdCnt .= "<span class='clrDay italics'>$refTxt . $responseRef </span>";
 			$rdCnt .= "<p><span class='clrDay italics' >பல்லவி:</span> $responseTxt $altResponse</p>";
-
-			//Format individual Verses
+			
+			// Format individual Verses
 			$rdCnt_temp = str_replace ( '℟', " - <span class='clrDay'>பல்லவி</span><br/><br/>§", $readingTxt ['Content'] );
 			$rdCnt_temp = explode ( '§', $rdCnt_temp );
-			$rdCnt_temp = array_filter ( $rdCnt_temp );//remove empty values
+			$rdCnt_temp = array_filter ( $rdCnt_temp ); // remove empty values
 			foreach ( $rdCnt_temp as &$value ) {
 				$value = trim ( $value );
 				$t = explode ( ' ', $value, 2 );
@@ -158,15 +157,12 @@ class TamilLectionaryHTML {
 			}
 			$rdCnt_temp = implode ( '', $rdCnt_temp );
 			
-			//Enclose the formated veses into psalmText class
+			// Enclose the formated veses into psalmText class
 			$rdCnt .= '<div class="psalmText">' . $rdCnt_temp . '</div>';
 			
-			//Find alternatives psalms available and mark them so
-			preg_match ( '/^\d\.(\d)$/', $readngType, $matches, PREG_OFFSET_CAPTURE );
-			if (! empty ( $matches )) {
-				if ($matches [1] [0] !== '1') {
-					$rdCnt = "<hr class='clrDay'/><h4 class='clrDay italics'>அல்லது</h4>" . $rdCnt;
-				}
+			// Find alternatives available and mark them so
+			if(isset($readngType[2]) && intval($readngType[2]) !== 0){
+				$rdCnt = "<hr class='clrDay'/><h4 class='clrDay italics'>அல்லது</h4>" . $rdCnt;
 			}
 			
 			$rt .= $rdCnt;
@@ -174,7 +170,38 @@ class TamilLectionaryHTML {
 		
 		return $rt;
 	}
-	
+	function getAlleluiaTxt($readings, $usedBy = null, $isLent = false) {
+		$rt = '';
+
+		// Set Heading
+		$rt .= "<h4 class='clrDay'>{$this->readingType[ intval( array_key_first ( $readings ) ) . $isLent]}</h4>";
+		
+		foreach ( $readings as $readngType => $value ) {
+			$readingTxt = $this->database->get ( 'readings__text', '*', [ 
+					'refKey' => $value,
+					'usedBy' => $usedBy 
+			] );
+			
+			$rdCnt = '';
+			if (TamilLectionaryUtil::expandBibleRef ( $value ) !== false) {
+				$rdCnt .= '<span>' . TamilLectionaryUtil::formatVerseToPrint ( $value ) . '</span>';
+			}
+			
+			if ($isLent) {
+				$rdCnt .= "<p class='alleluiaTxt'>{$readingTxt['Content']}</p>";
+			} else {
+				$rdCnt .= "<p class='alleluiaTxt'>அல்லேலூயா, அல்லேலூயா! {$readingTxt ['Content']} அல்லேலூயா.</p>";
+			}
+			
+			// Find alternatives available and mark them so
+			if(isset($readngType[2]) && intval($readngType[2]) !== 0){
+				$rdCnt = "<hr class='clrDay'/><h4 class='clrDay italics'>அல்லது</h4>" . $rdCnt;
+			}
+			$rt .= $rdCnt;
+		}
+		
+		return $rt;
+	}
 }
 
 ?>

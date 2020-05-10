08d95a10b6b6ca66d00cf9302dbe957e441f16e5
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2020-05-10 15:37:28 +0530

    added fromating function for Responsorial Psalms

--- lib/TamilLectionary/TamilLectionaryHTML.php
+++ lib/TamilLectionary/TamilLectionaryHTML.php
@@ -85,6 +85,7 @@ class TamilLectionaryHTML {
 			$rdCnt .= "<p class='readingTxt'>" . str_replace ( '§', '</p><p class="readingTxt">', $readingTxt ['Content'] ) . "</p>";
 			$rdCnt .= "<p class='readingTxt'>ஆண்டவரின் அருள்வாக்கு.</p>";
 			
+			//find out if alternative readings are available, and make them so 
 			preg_match ( '/^\d\.(\d)$/', $readngType, $matches, PREG_OFFSET_CAPTURE );
 			if (! empty ( $matches )) {
 				if ($matches [1] [0] !== '1') {
@@ -92,17 +93,16 @@ class TamilLectionaryHTML {
 				}
 			}
 			
+			//find out if short readings are available, and make them so 
 			preg_match ( '/^\d\.\d(\d)$/', $readngType, $matches, PREG_OFFSET_CAPTURE );
 			if (! empty ( $matches )) {
 				if ($matches [1] [0] !== '1')
 					$rdCnt = "<hr class='clrDay'/><h4 class='clrDay italics'>அல்லது குறுகிய வாசகம்</h4>" . $rdCnt;
 			}
-			
 			$rt .= $rdCnt;
 		}
 		return $rt;
 	}
-	
 	
 	function getResponsorialTxt($readings, $usedBy = null) {
 		$database = new Medoo ( [
@@ -123,16 +123,58 @@ class TamilLectionaryHTML {
 					'usedBy' => $usedBy
 			] );
 			
+			//Give spaing to verses
+			$refTxt = TamilLectionaryUtil::formatVerseToPrint ( $value );
+			
+			//If alternative response is set (அல்லது:...), then extract it.
+			$altResponse = '';
+			preg_match('/(.*)\( *அல்லது *:?(.*?)\)/', $readingTxt['introRes'], $matches);
+			if(!empty($matches)){
+				$readingTxt['introRes'] = $matches[1];
+				$altResponse =  "<br/><span class='clrDay'>அல்லது :</span>" . $matches[2];
+			}
+
+			//Seperate the response verse ref from responce text.
+			$responseTxt = $readingTxt['introRes'];
+			$responseRef = '';
+			preg_match('/(.*)§(.*)/', $readingTxt['introRes'], $matches);
+			if(!empty($matches)){
+				$responseTxt = $matches[1];
+				$responseRef = ' (பல்லவி: ' . $matches[2] .')';
+			}
 			
 			$rdCnt = '';
-			$rdCnt .= "<span class='readingHeading'>{$readingTxt['introRes']}</span><br/>";
-			$rdCnt .= "<div style='clear:both;'></div>";
+			$rdCnt .= '<span class="clrDay italics">' . $refTxt . $responseRef .'</span>';
+			$rdCnt .= "<p><span class='clrDay italics' >பல்லவி:</span> $responseTxt $altResponse</p>";
+
+			//Format individual Verses
+			$rdCnt_temp = str_replace ( '℟', " - <span class='clrDay'>பல்லவி</span><br/><br/>§", $readingTxt ['Content'] );
+			$rdCnt_temp = explode ( '§', $rdCnt_temp );
+			$rdCnt_temp = array_filter ( $rdCnt_temp );//remove empty values
+			foreach ( $rdCnt_temp as &$value ) {
+				$value = trim ( $value );
+				$t = explode ( ' ', $value, 2 );
+				$value = '<div>' . $t [0] . '</div><div>' . $t [1] . '</div>';
+			}
+			$rdCnt_temp = implode ( '', $rdCnt_temp );
 			
+			//Enclose the formated veses into psalmText class
+			$rdCnt .= '<div class="psalmText">' . $rdCnt_temp . '</div>';
+			
+			//Find alternatives psalms available and mark them so
+			preg_match ( '/^\d\.(\d)$/', $readngType, $matches, PREG_OFFSET_CAPTURE );
+			if (! empty ( $matches )) {
+				if ($matches [1] [0] !== '1') {
+					$rdCnt = "<hr class='clrDay'/><h4 class='clrDay italics'>அல்லது</h4>" . $rdCnt;
+				}
+			}
 			
 			$rt .= $rdCnt;
 		}
+		
 		return $rt;
 	}
+	
 }
 
 ?>

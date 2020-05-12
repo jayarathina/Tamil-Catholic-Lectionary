bee792ea3162b3a7c32cba9b4726f614bcff6853
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2020-05-12 19:15:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,49 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 include_once 'lib/TamilLectionary/TamilLectionary.php';
-include_once 'lib/TamilLectionary/TamilLectionaryHTML.php';
-
+include_once 'lib/TamilLectionary/TamilLectionaryUtil.php';
 ?>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="year.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	</head>
 <body>
-
+<table>
 <?php
+$year = date ( "Y" );
 
-$CalcGen = new TamilLectionary (date("Y"), parse_ini_file ( 'lib/RomanCalendar/settings.ini' ) );
+if (isset ( $_GET ['year'] )) {
+	if (preg_match ( "/^(19|20)\d{2}$/", $_GET ['year'] ) == 1) { // Years between 1900-2099
+		$year = $_GET ['year'];
+	}
+}
 
-/* * /
-print_r($CalcGen->fullYear);
-die();
-/* */
+$CalcGen = new TamilLectionary ( $year, parse_ini_file ( 'lib/RomanCalendar/settings.ini' ) );
 
-$printer = new TamilLectionaryHTML($CalcGen->fullYear);
-
-echo $printer->getDay(29, 3);
-
+//Print year with previous and next links
+$rows = "<tr><th colspan=3> <a class='arrowRight' href='index.php?year=" . ($year - 1) . "'>◄</a> $CalcGen->curYear <a class='arrowLeft' href='index.php?year=" . ($year + 1) . "'>►</a> </th></tr>";
+foreach ( $CalcGen->fullYear as $month => $value ) {
+	//Month Name
+	$rows .= '<tr> <th colspan="3">' . TamilLectionaryUtil::$tamilMonthFull [$month] . '</th> </tr>';
+	foreach ( $value as $days => $feasts ) {
+		foreach ( $feasts as $key => $fet ) {
+			$rows .= '<tr class="Col' . $fet ['color'] . '">';
+			if ($key == 0) {
+				$rows .= "<td class='dt' rowspan=" . sizeof ( $feasts ) . ">" . str_pad ( $days, 2, "0", STR_PAD_LEFT ) . "</td>";
+			}
+			
+			$type = isset ( $fet ['type'] ) ? ' (' . TamilLectionaryUtil::$tamilFeastType [$fet ['type']] . ')' : '';
+			
+			$rows .= "<td class='col ColD{$fet ['color']}'></td>";
+			$rows .= "<td><a class='dayTitle' href='ViewDay.php?d=$days&m=$month&y=$year&k=$key'>{$fet ['ta_name']}$type</a></td>";
+			$rows .= '</tr>';
+		}
+	}
+}
+echo $rows;
 
 ?>
+
+</table>
+</body>

1b0ebc3107df5016d0cb2f0ef6ef6985a4c33605
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2020-10-05 21:26:13 +0530

    gen fix

--- ViewDay.php
+++ ViewDay.php
@@ -1,5 +1,5 @@
 <?php
-header ( 'Content-Type: text/html; charset=utf-8' );
+header ('Content-Type: text/html; charset=utf-8' );
 include_once 'lib/TamilLectionary/TamilLectionary.php';
 include_once 'lib/TamilLectionary/TamilLectionaryHTML.php';
 
